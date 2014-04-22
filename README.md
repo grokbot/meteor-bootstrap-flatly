97bcd16bbe76a2dc8b1423cfa7ed697b185b5a3c
dramsey <dramsey@daytonmetrolibrary.org>
2014-04-22 09:12:24 -0400

    modified to use font-awesome instead of glyphicons. maybe broken...

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # meteor-bootstrap-flatly
-bootstrap 3 with flatly theme -  packaged for meteor   
+bootstrap 3 with flatly theme -  packaged for meteor - with font awesome instead of glyphicons
 [bootswatch - flatly](http://bootswatch.com/flatly/)   
    
 ### to install
       mrt add bootstrap-flatly
+
--- package.js
+++ package.js
@@ -10,14 +10,4 @@ Package.on_use(function (api) {
   api.add_files(path.join(asset_path, 'css', 'bootstrap.css'), 'client');
   api.add_files(path.join(asset_path, 'js', 'bootstrap.js'), 'client');
 
-  // fonts
-  api.add_files(path.join(asset_path, 'fonts', 'glyphicons-halflings-regular.eot'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'glyphicons-halflings-regular.ttf'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'glyphicons-halflings-regular.svg'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'glyphicons-halflings-regular.woff'), 'client');
-
-  // XXX this makes the paths to the icon sets absolute. it needs
-  // to be included _after_ the standard bootstrap css so
-  // that its styles take precedence.
-  api.add_files(path.join('bootstrap-override.css'), 'client');
 });
